9ef45c8184880d5b14dc10b18ac11e8644e7a7b7
yu23ki14 <yuki_021423@yahoo.co.jp>
2020-04-19 23:58:34 +0900

    update prod mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,19 +82,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {  host: ENV['API_URL'] }
   #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
   config.action_mailer.delivery_method = :smtp
   #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
   config.action_mailer.smtp_settings = {
     #gmail利用時はaddress,domain,portは下記で固定
     address:"smtp.gmail.com",
-    domain: 'gmail.com',
+    domain: 'heroku.com',
+    authentication: "plain"
     port:587,
     #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
     user_name: ENV['MAILUSERNAME'],
     #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-    password: ENV['MAILPASSWORD']
+    password: ENV['MAILPASSWORD'],
+    enable_starttls_auto: true
   }
 
   # Inserts middleware to perform automatic connection switching.
