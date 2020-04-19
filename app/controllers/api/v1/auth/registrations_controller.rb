d57033d9cee4f6cc68d14f648a23987029087b5b
yu23ki14 <yuki_021423@yahoo.co.jp>
2020-04-19 23:46:28 +0900

    tmp

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -10,7 +10,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   end
 
   private
-  # :company( 企業名 )を追加できるようにpravateメソッドに修正を加える
   def sign_up_params
     params.require(:registration).permit(:name, :email, :phone_number, :password, :password_confirmation)
   end
